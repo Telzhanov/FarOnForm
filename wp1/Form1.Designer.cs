3fe1b4caa40eaf1880f16b1b78c7dec18bbb55d7
Telzhanov <askhat491@gmail.com>
2017-05-20 00:17:20 +0600

    Add project files.

---
+++ wp1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace wp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Name1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Name1});
+            this.listView1.Location = new System.Drawing.Point(12, 40);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(406, 113);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Tile;
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView1_ItemSelectionChanged);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.listView1_KeyPress);
+            // 
+            // Name1
+            // 
+            this.Name1.Text = "Name";
+            this.Name1.Width = 110;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 321);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Name1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ wp1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wp1
+{
+    public partial class Form1 : Form
+    {
+        int counter;
+        Stack<string> st = new Stack<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            DirectoryInfo di = new DirectoryInfo(@"C:\");
+            DirectoryInfo[] x = di.GetDirectories();
+            for (int i = 0; i < x.Length; ++i)
+            {
+                listView1.Items.Add(x[i].FullName);
+                
+            }
+            
+        }
+
+        private void listView1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+            if (e.KeyChar==(char)Keys.Back)
+            {
+                st.Pop();
+                int count = listView1.Items.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    listView1.Items[0].Remove();
+                }
+                if (st.Count==0)
+                {
+                    st.Push(@"C:\");
+                }
+                DirectoryInfo di = new DirectoryInfo(@"" + st.Peek());
+                DirectoryInfo[] x = di.GetDirectories();
+                for (int i = 0; i < x.Length; ++i)
+                {
+                    listView1.Items.Add(x[i].FullName);
+
+                }
+
+            }
+            if (e.KeyChar==(char)Keys.Enter)
+            {
+                 int count = listView1.Items.Count;
+                for (int i=0; i<listView1.Items.Count; i++)
+                {
+                    if (listView1.Items[i].Selected==true)
+                    {
+                        counter = i;
+                    }
+                }
+                string path = listView1.Items[counter].Text;
+                st.Push(path);
+                for (int i=0; i<count; i++)
+                {
+                    listView1.Items[0].Remove();
+                }
+                DirectoryInfo di = new DirectoryInfo(@""+path);
+                DirectoryInfo[] x = di.GetDirectories();
+                for (int i = 0; i < x.Length; ++i)
+                {
+                    listView1.Items.Add(x[i].FullName);
+                   
+
+                }
+            }
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
